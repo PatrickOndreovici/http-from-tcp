050170c7c8b17b0c92f6ebc44c2d6f54cef42c14
Patrick Ondreovici <ondreovicip@gmail.com>
2026-07-26 21:38:51 +0300

    save progress

--- internal/request/request.go
+++ internal/request/request.go
@@ -18,42 +18,69 @@ type RequestLine struct {
 	Method        string
 }
 
+const CLRF = "\r\n"
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	buf := make([]byte, 4096)
+	buf := make([]byte, 8)
 
-	line := ""
+	encounterClrf := false
+	readIndex := 0
+	currentClrfIndex := 0
+	lineEnd := -1
 
-	for {
-		n, err := reader.Read(buf)
-
-		lines := strings.Split(string(buf[:n]), "\r\n")
-
-		if len(lines) > 1 {
-			line += lines[0]
-			break
-		} else {
-			line += lines[0]
+	for !encounterClrf {
+		n, err := reader.Read(buf[readIndex:])
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
 		}
-		if err == io.EOF {
-			break
+		if n == 0 {
+			return nil, errors.New("invalid request line: " + string(buf[:n]) + "")
+		}
+		for i := readIndex; i < n+readIndex; i++ {
+			if buf[i] == CLRF[currentClrfIndex] {
+				currentClrfIndex++
+			} else {
+				if buf[i] == CLRF[0] {
+					currentClrfIndex = 1
+				} else {
+					currentClrfIndex = 0
+				}
+			}
+			if currentClrfIndex == len(CLRF) {
+				lineEnd = i - 1
+				encounterClrf = true
+				break
+			}
+		}
+		readIndex += n
+		if readIndex >= len(buf) {
+			newBuf := make([]byte, len(buf)*2)
+			copy(newBuf, buf)
+			buf = newBuf
 		}
 
 	}
+	line := string(buf[:lineEnd])
 
 	parts := strings.Split(line, " ")
 	if len(parts) != 3 {
-		return nil, errors.New("invalid request line: " + line + "")
+		return nil, errors.New("invalid request line: " + line)
 	}
+
 	if parts[2] != "HTTP/1.1" || parts[1][0] != '/' {
-		return nil, errors.New("invalid request line: " + line + "")
+		return nil, errors.New("invalid request line: " + line)
 	}
+
 	request := &Request{
 		Headers: make(map[string]string),
 		Body:    []byte{},
 		RequestLine: RequestLine{
-			HttpVersion:   strings.Split(parts[2], "/")[1],
-			RequestTarget: parts[1],
 			Method:        parts[0],
+			RequestTarget: parts[1],
+			HttpVersion:   "1.1",
 		},
 	}
 
